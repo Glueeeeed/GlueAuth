3c1b594c1242fb2ec9c75f21530547183f0397f4
Glueeed <swiderskikuba12@gmail.com>
2026-02-10 07:04:13 +0100

    Initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+    root: './',
+    build: {
+        outDir: '../src/public',
+        rollupOptions: {
+            input: {
+                login: resolve(__dirname, 'login.html'),
+                register: resolve(__dirname, 'register.html'),
+            },
+        },
+    },
+})
